39d7629815fe07f3aad28a4b6395d1c8d3de0373
Adrian Strat <adrian.stratienco@Rackspace.com>
2023-07-27 12:01:58 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
